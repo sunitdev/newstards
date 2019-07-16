f4073f1e0cc2750156bb17543c0bd84bc2dd1c50
Sunit Deshpande <sunitdeshpande1234@gmail.com>
2019-07-16 13:39:44 +0530

    Added reddit list loading animation

--- app/src/main/java/com/news/aggregator/newstard/ui/adapters/reddit/viewholders/RedditLoadingViewHolder.kt
+++ app/src/main/java/com/news/aggregator/newstard/ui/adapters/reddit/viewholders/RedditLoadingViewHolder.kt
@@ -3,5 +3,14 @@ package com.news.aggregator.newstard.ui.adapters.reddit.viewholders
 import androidx.recyclerview.widget.RecyclerView
 import com.news.aggregator.newstard.databinding.FragmentRedditListLoadingLayoutBinding
 
-class RedditLoadingViewHolder(layoutBinding: FragmentRedditListLoadingLayoutBinding):
-    RecyclerView.ViewHolder(layoutBinding.root)
+class RedditLoadingViewHolder(private val _layoutBinding: FragmentRedditListLoadingLayoutBinding):
+    RecyclerView.ViewHolder(_layoutBinding.root) {
+
+    fun startAnimation(){
+        _layoutBinding.redditLoadingShimmerLayout.startShimmerAnimation()
+    }
+
+    fun stopAnimation() {
+        _layoutBinding.redditLoadingShimmerLayout.stopShimmerAnimation()
+    }
+}
