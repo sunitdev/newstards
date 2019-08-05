940568f33f55585dc320993a63c4d2d6a4522d83
Sunit Deshpande <sunitdeshpande1234@gmail.com>
2019-08-05 14:59:15 +0530

    Added ServiceViewModel class

--- app/src/main/java/com/news/aggregator/newstard/ui/pagination/base/ServiceDataSourceFactory.kt
+++ app/src/main/java/com/news/aggregator/newstard/ui/pagination/base/ServiceDataSourceFactory.kt
@@ -3,8 +3,9 @@ package com.news.aggregator.newstard.ui.pagination.base
 import androidx.lifecycle.MutableLiveData
 import androidx.paging.DataSource
 
-abstract class ServiceDataSourceFactory<Key, PostModelClass, DataSourceClass: DataSource<Key, PostModelClass>>:
-    DataSource.Factory<Key, PostModelClass>() {
+abstract class ServiceDataSourceFactory<Key, PostModelClass, DataSourceClass>:
+    DataSource.Factory<Key, PostModelClass>()
+    where DataSourceClass: DataSource<Key, PostModelClass> {
 
     private val _dataSourceLiveData = MutableLiveData<DataSourceClass>()
 
